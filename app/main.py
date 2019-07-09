e7a8a6c84d194700ec149eb54d3a0fffd945ad6d
george <george@georgebh.com>
2019-07-09 18:46:51 +0100

    add you are + i am

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/", responce_class=HTMLResponse)
-def read_root():
-    return "<h1> HELLO <h1/>
+def format_home_page():
+    return "dog"
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(*, user_agent: str = Header(None), host: str = Header(None)):
+    welcome_msg = """
+    <h1> HELLO </h1>
+    <p> <b> You are : </b>""" + user_agent + "</p>" + \
+    "<b> I am: </b>" + host
+
+    return welcome_msg 
