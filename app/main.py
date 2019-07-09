c33c3b847b7f871f30338c3b1b4c166abf43c69e
george <george@georgebh.com>
2019-07-09 17:02:35 +0100

    test html response

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", responce_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    return "<h1> HELLO <h1/>
